c7d1a6b9ec9cef7848b40e448eb42acd4105d0aa
4anth0ny1 <acatullo4@gmail.com>
2020-12-14 18:21:07 -0600

    commit tree bc7fc6c226129a24675b76b08c8656092f14ebec

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+    i = 0
+    collection_new = []
+    while i < collection.length do
+      collection_new << yield(collection[i])
+      i = i + 1 
+    end
+    collection_new
+end
 
